92caa2b63cf8c610066eebfa99bb3a96f31518df
Mary Goodhart <marya.grace5@gmail.com>
2018-11-21 11:30:28 -0700

    a huge commit that should've been made in smaller chunks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,14 @@
 require 'sqlite3'
 
 class Task
+  attr_reader :title, :description, :id
 
-  attr_reader :title, :description
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
+    @id = task_params["id"] if task_params["id"]
   end
 
   def save
@@ -15,11 +16,34 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
     end
   end
+
+  def self.find(id)
+    task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
+    Task.new(task)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def self.update(id, task_params)
+    database.execute("UPDATE tasks
+                      SET title = ?,
+                          description = ?
+                      WHERE id = ?;",
+                      task_params[:title],
+                      task_params[:description],
+                      id)
+
+    Task.find(id)
+  end
+
+  
 end
